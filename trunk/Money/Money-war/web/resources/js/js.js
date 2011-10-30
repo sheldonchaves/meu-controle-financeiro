5d38fdb239062ddf1e4f3f20a59544e3383ba0c2
gbvbahia01@gmail.com <gbvbahia01@gmail.com@133ebd45-cf3e-885a-2d9d-162f8e9bdabc>
2011-10-30 22:14:19 +0000

    validação de data

--- trunk/Money/Money-war/web/resources/js/js.js
+++ trunk/Money/Money-war/web/resources/js/js.js
@@ -39,7 +39,7 @@ function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){
         objTextBox.value = '';
         len2 = aux2.length;
         for (i = len2 - 1; i >= 0; i--)
-        objTextBox.value += aux2.charAt(i);
+            objTextBox.value += aux2.charAt(i);
         objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);
     }
     return false;
@@ -57,12 +57,12 @@ function verificaEmail(msg){
 
 function getInternetExplorer()  
 {  
-   if (navigator.appName == 'Microsoft Internet Explorer')  
-   {  
-       alert('Sorry, this site is not compatible with Internet Explorer.\nPlease use Google Chrome or Mozilla FireFox.');
-     return false;  
-   }  
-   return true;  
+    if (navigator.appName == 'Microsoft Internet Explorer')  
+    {  
+        alert('Sorry, this site is not compatible with Internet Explorer.\nPlease use Google Chrome or Mozilla FireFox.');
+        return false;  
+    }  
+    return true;  
 }  
 function confirme(msg){
     apagar = confirm(msg)//a variavel apagar aguarda um comando ok ou cancelar retornando assim false ou true
@@ -86,15 +86,15 @@ function Contar(Campo, idmsg, limite){
     }
 }
 function formatar(src, mask, e){
-  var whichCode = (window.Event) ? e.which : e.keyCode;
-  if(whichCode == 13 || whichCode == 8 || whichCode == 0) return true;
-  var i = src.value.length;
-  var saida = mask.substring(0,1);
-  var texto = mask.substring(i)
-if (texto.substring(0,1) != saida)
-  {
-    src.value += texto.substring(0,1);
-  }
+    var whichCode = (window.Event) ? e.which : e.keyCode;
+    if(whichCode == 13 || whichCode == 8 || whichCode == 0) return true;
+    var i = src.value.length;
+    var saida = mask.substring(0,1);
+    var texto = mask.substring(i)
+    if (texto.substring(0,1) != saida)
+    {
+        src.value += texto.substring(0,1);
+    }
 }
 
 //onkeypress='return SomenteNumero(event)'
@@ -102,42 +102,157 @@ function SomenteNumero(e){
     var tecla=(window.event)?event.keyCode:e.which;   
     if((tecla>47 && tecla<58)) return true;
     else{
-    	if (tecla==8 || tecla==0) return true;
-	else  return false;
+        if (tecla==8 || tecla==0) return true;
+        else  return false;
     }
 }
-/**
-function getInternetExplorerVersion()
-// Returns the version of Windows Internet Explorer or a -1
-// (indicating the use of another browser).
-{
-   var rv = -1; // Return value assumes failure.
-   if (navigator.appName == 'Microsoft Internet Explorer')
-   {
-      var ua = navigator.userAgent;
-      var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-      if (re.exec(ua) != null)
-         rv = parseFloat( RegExp.$1 );
-   }
-   return rv;
+function MascaraCNPJ(cnpj){
+    if(mascaraInteiro(cnpj)==false){
+        event.returnValue = false;
+    }       
+    return formataCampo(cnpj, '00.000.000/0000-00', event);
 }
-function checkIEVersion()
-{
-   var ver = getInternetExplorerVersion();
-   if ( ver > -1 )
-   {
-         if(ver == "9"){
-             alert('true');
-             return true;
-         }
-         else{
-             alert('false');
-             return false;
-         }
-   }
-   else{
-       return false;
-   }
+
+//adiciona mascara de cep
+function MascaraCep(cep){
+    if(mascaraInteiro(cep)==false){
+        event.returnValue = false;
+    }       
+    return formataCampo(cep, '00.000-000', event);
 }
-*/
+
+//adiciona mascara de data
+function MascaraData(data){
+    if(mascaraInteiro(data)==false){
+        event.returnValue = false;
+    }
+    return formataCampo(data, '00/00/0000', event);
+}
+
+//adiciona mascara ao telefone
+function MascaraTelefone(tel){  
+    if(mascaraInteiro(tel)==false){
+        event.returnValue = false;
+    }       
+    return formataCampo(tel, '(00) 0000-0000', event);
+}
+
+//adiciona mascara ao CPF
+function MascaraCPF(cpf){
+    if(mascaraInteiro(cpf)==false){
+        event.returnValue = false;
+    }       
+    return formataCampo(cpf, '000.000.000-00', event);
+}
+
+//valida telefone
+function ValidaTelefone(tel){
+    exp = /\(\d{2}\)\ \d{4}\-\d{4}/
+    if(!exp.test(tel.value))
+        alert('Numero de Telefone Invalido!');
+}
+
+//valida CEP
+function ValidaCep(cep){
+    exp = /\d{2}\.\d{3}\-\d{3}/
+    if(!exp.test(cep.value))
+        alert('Numero de Cep Invalido!');               
+}
+
+//valida data
+function ValidaData(data){
+    exp = /\d{2}\/\d{2}\/\d{4}/
+    if(!exp.test(data.value)){
+        data.value = "";
+        alert('Data Invalida!');
+        data.select();          
+    }
+}
+//valida o CPF digitado
+function ValidarCPF(Objcpf){
+    var cpf = Objcpf.value;
+    exp = /\.|\-/g
+    cpf = cpf.toString().replace( exp, "" ); 
+    var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
+    var soma1=0, soma2=0;
+    var vlr =11;
+        
+    for(i=0;i<9;i++){
+        soma1+=eval(cpf.charAt(i)*(vlr-1));
+        soma2+=eval(cpf.charAt(i)*vlr);
+        vlr--;
+    }       
+    soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
+    soma2=(((soma2+(2*soma1))*10)%11);
+        
+    var digitoGerado=(soma1*10)+soma2;
+    if(digitoGerado!=digitoDigitado)        
+        alert('CPF Invalido!');         
+}
+
+//valida numero inteiro com mascara
+function mascaraInteiro(){
+    if (event.keyCode < 48 || event.keyCode > 57){
+        event.returnValue = false;
+        return false;
+    }
+    return true;
+}
+
+//valida o CNPJ digitado
+function ValidarCNPJ(ObjCnpj){
+    var cnpj = ObjCnpj.value;
+    var valida = new Array(6,5,4,3,2,9,8,7,6,5,4,3,2);
+    var dig1= new Number;
+    var dig2= new Number;
+        
+    exp = /\.|\-|\//g
+    cnpj = cnpj.toString().replace( exp, "" ); 
+    var digito = new Number(eval(cnpj.charAt(12)+cnpj.charAt(13)));
+                
+    for(i = 0; i<valida.length; i++){
+        dig1 += (i>0? (cnpj.charAt(i-1)*valida[i]):0);  
+        dig2 += cnpj.charAt(i)*valida[i];       
+    }
+    dig1 = (((dig1%11)<2)? 0:(11-(dig1%11)));
+    dig2 = (((dig2%11)<2)? 0:(11-(dig2%11)));
+        
+    if(((dig1*10)+dig2) != digito)  
+        alert('CNPJ Invalido!');
+                
+}
+
+//formata de forma generica os campos
+function formataCampo(campo, Mascara, evento) { 
+    var boleanoMascara; 
+        
+    var Digitato = evento.keyCode;
+    exp = /\-|\.|\/|\(|\)| /g
+    campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+   
+    var posicaoCampo = 0;    
+    var NovoValorCampo="";
+    var TamanhoMascara = campoSoNumeros.length;
+    ; 
+        
+    if (Digitato != 8) { // backspace 
+        for(i=0; i<= TamanhoMascara; i++) { 
+            boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                || (Mascara.charAt(i) == "/")) 
+            boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
+                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
+            if (boleanoMascara) { 
+                NovoValorCampo += Mascara.charAt(i); 
+                TamanhoMascara++;
+            }else { 
+                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+                posicaoCampo++; 
+            }              
+        }      
+        campo.value = NovoValorCampo;
+        return true; 
+    }else { 
+        return true; 
+    }
+}
 //]]>
