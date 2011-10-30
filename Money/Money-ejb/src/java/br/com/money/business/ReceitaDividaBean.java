07f5c459cf5f5c96efaaa7e09def4276c93ad4e0
gbvbahia01 <gbvbahia01@gmail.com>
2011-10-30 21:41:37 +0000

    deleção de receita divida

--- Money/Money-ejb/src/java/br/com/money/business/ReceitaDividaBean.java
+++ Money/Money-ejb/src/java/br/com/money/business/ReceitaDividaBean.java
@@ -7,6 +7,7 @@ package br.com.money.business;
 import br.com.money.business.interfaces.ReceitaDividaBeanLocal;
 import br.com.money.enums.StatusPagamento;
 import br.com.money.enums.TipoMovimentacao;
+import br.com.money.exceptions.ReceitaDividaException;
 import br.com.money.exceptions.ValidacaoException;
 import br.com.money.modelos.ReceitaDivida;
 import br.com.money.modelos.Usuario;
@@ -15,33 +16,33 @@ import java.util.*;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.*;
+
 /**
  *
  * @author Guilherme
  */
 @Stateless
 public class ReceitaDividaBean implements ReceitaDividaBeanLocal {
-    
-    @EJB(beanName="receitaDividaValidador")
-    private ValidadorInterface receitaDividaValidador;
 
-     @PersistenceContext(name = "jdbc/money")
+    @EJB(beanName = "receitaDividaValidador")
+    private ValidadorInterface receitaDividaValidador;
+    @PersistenceContext(name = "jdbc/money")
     private EntityManager manager;
-     
+
     @Override
-    public void salvarReceitaDivida(ReceitaDivida conta) throws ValidacaoException{
+    public void salvarReceitaDivida(ReceitaDivida conta) throws ValidacaoException {
         this.receitaDividaValidador.validar(conta, this, null);
-        if(conta.getId() == null){
+        if (conta.getId() == null) {
             manager.persist(conta);
-        }else{
+        } else {
             manager.merge(conta);
         }
         manager.flush();
     }
 
     @Override
-    public List<ReceitaDivida> buscarReceitaDividasPorUsuarioStatusPaginada(int posicaoInicial, int tamanho, 
-    Usuario usuario, StatusPagamento statusPagamento, TipoMovimentacao tipoMovimentacao) {
+    public List<ReceitaDivida> buscarReceitaDividasPorUsuarioStatusPaginada(int posicaoInicial, int tamanho,
+            Usuario usuario, StatusPagamento statusPagamento, TipoMovimentacao tipoMovimentacao) {
         Query q = manager.createNamedQuery("ReceitaDividaBean.buscarReceitaDividasPorUsuarioStatusPaginada");
         q.setMaxResults(tamanho);
         q.setFirstResult(posicaoInicial);
@@ -50,9 +51,9 @@ public class ReceitaDividaBean implements ReceitaDividaBeanLocal {
         q.setParameter("tipoMovimentacao", tipoMovimentacao);
         return q.getResultList();
     }
-    
+
     @Override
-    public Integer buscarQutdadeReceitaDividasPorUsuarioStatusPaginada(Usuario usuario, StatusPagamento statusPagamento, TipoMovimentacao tipoMovimentacao){
+    public Integer buscarQutdadeReceitaDividasPorUsuarioStatusPaginada(Usuario usuario, StatusPagamento statusPagamento, TipoMovimentacao tipoMovimentacao) {
         Query q = manager.createNamedQuery("ReceitaDividaBean.buscarQutdadeReceitaDividasPorUsuarioStatusPaginada");
         q.setParameter("usuario", usuario);
         q.setParameter("statusPagamento", statusPagamento);
@@ -60,6 +61,22 @@ public class ReceitaDividaBean implements ReceitaDividaBeanLocal {
         Long toReturn = (Long) q.getSingleResult();
         return toReturn.intValue();
     }
-    
-    
+
+    @Override
+    public void apagarReceitaDivida(ReceitaDivida receitaDivida, boolean deleteParcelas) {
+        ReceitaDivida rd = manager.find(ReceitaDivida.class, receitaDivida.getId());
+        if (rd.getStatusPagamento().equals(StatusPagamento.PAGA)) {
+            throw new ReceitaDividaException("deleteReceitaDividaPaga");
+        }
+        manager.remove(rd);
+        if (deleteParcelas) {
+            Query q = manager.createNamedQuery("ReceitaDividaBean.apagarReceitaDivida");
+            q.setParameter("usuario", rd.getUsuario());
+            q.setParameter("statusPagamento", StatusPagamento.NAO_PAGA);
+            q.setParameter("identificador", rd.getIdentificador());
+            q.setParameter("parcelaAtual", rd.getParcelaAtual());
+            q.executeUpdate();
+        }
+        manager.flush();
+    }
 }
