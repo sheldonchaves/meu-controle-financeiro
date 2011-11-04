f6b2846d979dce0fbf28d356f4be693e7217873b
gbvbahia01@gmail.com <gbvbahia01@gmail.com@133ebd45-cf3e-885a-2d9d-162f8e9bdabc>
2011-11-04 01:42:15 +0000

    Metodos para busca de movimentação financeira

--- trunk/Money/Money-ejb/src/java/br/com/money/business/interfaces/MovimentacaoFinanceiraBeanLocal.java
+++ trunk/Money/Money-ejb/src/java/br/com/money/business/interfaces/MovimentacaoFinanceiraBeanLocal.java
@@ -5,7 +5,10 @@
 package br.com.money.business.interfaces;
 
 import br.com.money.modelos.ContaBancaria;
+import br.com.money.modelos.MovimentacaoFinanceira;
 import br.com.money.modelos.ReceitaDivida;
+import br.com.money.modelos.Usuario;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -14,5 +17,27 @@ import javax.ejb.Local;
  */
 @Local
 public interface MovimentacaoFinanceiraBeanLocal {
+    /**
+     * Cria a movimentação financeira para a conta e a receita passada.
+     * @param contaBancaria
+     * @param receitaDivida 
+     */
     public void salvarMovimentacaoFinanceira(ContaBancaria contaBancaria, ReceitaDivida receitaDivida);
+    /**
+     * Retornar as movimentações financeiras de um usuário ou conjuge do mesmo.
+     * Páginada, por ser um grande volume de dados
+     * @param posicaoInicial
+     * @param tamanho Quantidade máxima de registros que serão retornados.
+     * @param usuario
+     * @return 
+     */
+    public List<MovimentacaoFinanceira> buscarMovimentacaoPorUsuarioStatusPaginada(int posicaoInicial, int tamanho, Usuario usuario);
+    
+    /**
+     * Retorna a quantidade de registros de movimentação financeira de um determinado usuário.
+     * Necessário para controle da paginação
+     * @param usuario
+     * @return 
+     */
+    public Integer buscarQtdadeMovimentacaoPorUsuarioStatusPaginada(Usuario usuario);
 }
