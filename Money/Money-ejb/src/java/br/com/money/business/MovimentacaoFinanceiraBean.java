37d8a5a508a717342da979c9d1aa354065458a42
gbvbahia01 <gbvbahia01@gmail.com>
2011-11-04 01:42:15 +0000

    Metodos para busca de movimentação financeira

--- Money/Money-ejb/src/java/br/com/money/business/MovimentacaoFinanceiraBean.java
+++ Money/Money-ejb/src/java/br/com/money/business/MovimentacaoFinanceiraBean.java
@@ -11,11 +11,14 @@ import br.com.money.enums.StatusPagamento;
 import br.com.money.modelos.ContaBancaria;
 import br.com.money.modelos.MovimentacaoFinanceira;
 import br.com.money.modelos.ReceitaDivida;
+import br.com.money.modelos.Usuario;
 import br.com.money.vaidators.interfaces.ValidadorInterface;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -23,21 +26,18 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class MovimentacaoFinanceiraBean implements MovimentacaoFinanceiraBeanLocal {
-    
+
     @EJB
     private ReceitaDividaBeanLocal receitaDividaBean;
-    
     @EJB
     private ContaBancariaBeanLocal contaBancariaBean;
-
     @EJB(beanName = "movimentacaoFinanceiraValidador")
     private ValidadorInterface movimentacaoFinanceiraValidador;
-    
     @PersistenceContext(name = "jdbc/money")
     private EntityManager manager;
-    
+
     @Override
-    public void salvarMovimentacaoFinanceira(ContaBancaria contaBancaria, ReceitaDivida receitaDivida){
+    public void salvarMovimentacaoFinanceira(ContaBancaria contaBancaria, ReceitaDivida receitaDivida) {
         ContaBancaria cb = manager.find(ContaBancaria.class, contaBancaria.getId());
         ReceitaDivida rd = manager.find(ReceitaDivida.class, receitaDivida.getId());
         rd.setValor(receitaDivida.getValor());
@@ -47,11 +47,28 @@ public class MovimentacaoFinanceiraBean implements MovimentacaoFinanceiraBeanLoc
         this.movimentacaoFinanceiraValidador.validar(mf, this, null);
         this.contaBancariaBean.salvarContaBancaria(cb);
         this.receitaDividaBean.salvarReceitaDivida(rd);
-        if(mf.getId() == null){
+        if (mf.getId() == null) {
             manager.persist(mf);
-        }else{
+        } else {
             manager.merge(mf);
         }
         manager.flush();
+    }
+
+    @Override
+    public List<MovimentacaoFinanceira> buscarMovimentacaoPorUsuarioStatusPaginada(int posicaoInicial, int tamanho, Usuario usuario) {
+        Query q = manager.createNamedQuery("ReceitaDividaBean.buscarReceitaDividasPorUsuarioStatusPaginada");
+        q.setMaxResults(tamanho);
+        q.setFirstResult(posicaoInicial);
+        q.setParameter("usuario", usuario);
+        return q.getResultList();
+    }
+
+    @Override
+    public Integer buscarQtdadeMovimentacaoPorUsuarioStatusPaginada(Usuario usuario) {
+        Query q = manager.createNamedQuery("ReceitaDividaBean.buscarQtdadeMovimentacaoPorUsuarioStatusPaginada");
+        q.setParameter("usuario", usuario);
+        Long toReturn = (Long) q.getSingleResult();
+        return toReturn.intValue();
     }
 }
