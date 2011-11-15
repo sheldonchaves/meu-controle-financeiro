a989d9aebc4cdde43ad2ba622ab625cbdbc1c112
gbvbahia01 <gbvbahia01@gmail.com>
2011-11-15 15:56:16 +0000

    layout menu superior thema

--- Money/Money-war/src/java/br/com/gbvbahia/money/manager/SelectItemManager.java
+++ Money/Money-war/src/java/br/com/gbvbahia/money/manager/SelectItemManager.java
@@ -96,4 +96,46 @@ public class SelectItemManager implements Serializable{
         }
         return toReturn;
     }
+//    
+//     private Map<String, String> themes;  
+//    @PostConstruct  
+//    public void init() {  
+//          
+//        themes = new TreeMap<String, String>();  
+//        themes.put("Aristo", "aristo");  
+//        themes.put("Black-Tie", "black-tie");  
+//        themes.put("Blitzer", "blitzer");  
+//        themes.put("Bluesky", "bluesky");  
+//        themes.put("Casablanca", "casablanca");  
+//        themes.put("Cupertino", "cupertino");  
+//        themes.put("Dark-Hive", "dark-hive");  
+//        themes.put("Dot-Luv", "dot-luv");  
+//        themes.put("Eggplant", "eggplant");  
+//        themes.put("Excite-Bike", "excite-bike");  
+//        themes.put("Flick", "flick");  
+//        themes.put("Glass-X", "glass-x");  
+//        themes.put("Hot-Sneaks", "hot-sneaks");  
+//        themes.put("Humanity", "humanity");  
+//        themes.put("Le-Frog", "le-frog");  
+//        themes.put("Midnight", "midnight");  
+//        themes.put("Mint-Choc", "mint-choc");  
+//        themes.put("Overcast", "overcast");  
+//        themes.put("Pepper-Grinder", "pepper-grinder");  
+//        themes.put("Redmond", "redmond");  
+//        themes.put("Rocket", "rocket");  
+//        themes.put("Sam", "sam");  
+//        themes.put("Smoothness", "smoothness");  
+//        themes.put("South-Street", "south-street");  
+//        themes.put("Start", "start");  
+//        themes.put("Sunny", "sunny");  
+//        themes.put("Swanky-Purse", "swanky-purse");  
+//        themes.put("Trontastic", "trontastic");  
+//        themes.put("UI-Darkness", "ui-darkness");  
+//        themes.put("UI-Lightness", "ui-lightness");  
+//        themes.put("Vader", "vader");  
+//    }  
+//    
+//    public Map<String, String> getThemes() {  
+//        return themes;  
+//    } 
 }
