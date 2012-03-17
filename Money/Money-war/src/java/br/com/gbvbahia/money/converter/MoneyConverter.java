dee8de3dbf17f553d51be97f4bbd6bf94373fa08
gbvbahia01 <gbvbahia01@gmail.com>
2012-03-17 23:25:23 +0000

    refatoração do template divisão das páginas em 3 crud contabancaria completo

--- Money/Money-war/src/java/br/com/gbvbahia/money/converter/MoneyConverter.java
+++ Money/Money-war/src/java/br/com/gbvbahia/money/converter/MoneyConverter.java
@@ -4,7 +4,6 @@
  */
 package br.com.gbvbahia.money.converter;
 
-
 import br.com.gbvbahia.money.utils.MensagemUtils;
 import br.com.gbvbahia.money.utils.UtilMetodos;
 import javax.faces.application.FacesMessage;
@@ -27,15 +26,26 @@ public class MoneyConverter implements Converter {
         if (string == null || string.equals("") || StringUtils.isBlank(string)) {
             return string;
         }
+        boolean negativo = StringUtils.contains(string, "-");
+        if (negativo) {
+            string = StringUtils.remove(string, "-");
+            string = StringUtils.trim(string);
+        }
         if (StringUtils.substringBefore(string, ",").contains(",")) {
             exception(fc);
-        }else if(!StringUtils.contains(string, ",") && !StringUtils.contains(string, ".")){
+        } else if (!StringUtils.contains(string, ",") && !StringUtils.contains(string, ".")) {
             string += ",00";
-        }else if(!StringUtils.contains(string, ",") && StringUtils.contains(string, ".")){
+        } else if (!StringUtils.contains(string, ",") && StringUtils.contains(string, ".")) {
             string = string.replace(".", ",");
         }
         try {
-            return new Double(string.replace(".", "").replace(",", ".").trim());
+            Double toReturn =
+                    new Double(string.replace(".", "").replace(",", ".").trim());
+            if (negativo) {
+                return toReturn * -1;
+            } else {
+                return toReturn;
+            }
         } catch (Exception e) {
             exception(fc);
             return null;
@@ -50,7 +60,9 @@ public class MoneyConverter implements Converter {
 
     @Override
     public String getAsString(FacesContext fc, UIComponent uic, Object o) {
-        if(o == null ||StringUtils.isBlank(o.toString()) ) return "0,00";
+        if (o == null || StringUtils.isBlank(o.toString())) {
+            return "0,00";
+        }
 
         Double money = (Double) o;
         return UtilMetodos.currencyFormat(money).replace("R$", "").trim();
