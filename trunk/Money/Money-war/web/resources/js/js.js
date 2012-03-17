3d89a49831f38de69b0e5b7db22c535f8718d927
gbvbahia01@gmail.com <gbvbahia01@gmail.com@133ebd45-cf3e-885a-2d9d-162f8e9bdabc>
2012-03-17 23:16:09 +0000

    refatoração do template divisão das páginas em 3 crud contabancaria completo

--- trunk/Money/Money-war/web/resources/js/js.js
+++ trunk/Money/Money-war/web/resources/js/js.js
@@ -106,6 +106,25 @@ function SomenteNumero(e){
 	else  return false;
     }
 }
+
+function focusCampo(id){
+    var inputField = document.getElementById(id);
+    if (inputField != null && inputField.value.length != 0){
+        if (inputField.createTextRange){
+            var FieldRange = inputField.createTextRange();
+            FieldRange.moveStart('character',inputField.value.length);
+            FieldRange.collapse();
+            FieldRange.select();
+        }else if (inputField.selectionStart || inputField.selectionStart == '0') {
+            var elemLen = inputField.value.length;
+            inputField.selectionStart = elemLen;
+            inputField.selectionEnd = elemLen;
+            inputField.focus();
+        }
+    }else{
+        inputField.focus();
+    }
+}
 /**
 function getInternetExplorerVersion()
 // Returns the version of Windows Internet Explorer or a -1
