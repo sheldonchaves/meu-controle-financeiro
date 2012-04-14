9dee3f2dd528e1e281c0389abe78a61f4a29ea69
gbvbahia01@gmail.com <gbvbahia01@gmail.com@133ebd45-cf3e-885a-2d9d-162f8e9bdabc>
2012-04-14 15:11:31 +0000

    DespesaParcelada Entidade DespesaParcelada Criacao Teste Criacao

--- trunk/Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/ProcedimentoBean.java
+++ trunk/Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/ProcedimentoBean.java
@@ -5,12 +5,16 @@
 package br.com.gbvbahia.financeiro.beans;
 
 import br.com.gbvbahia.financeiro.beans.commons.AbstractFacade;
+import br.com.gbvbahia.financeiro.beans.exceptions.NegocioException;
 import br.com.gbvbahia.financeiro.beans.facades.ProcedimentoFacade;
 import br.com.gbvbahia.financeiro.constantes.StatusPagamento;
 import br.com.gbvbahia.financeiro.modelos.CartaoCredito;
+import br.com.gbvbahia.financeiro.modelos.DespesaParceladaProcedimento;
 import br.com.gbvbahia.financeiro.modelos.DespesaProcedimento;
 import br.com.gbvbahia.financeiro.modelos.Usuario;
 import br.com.gbvbahia.financeiro.modelos.superclass.Procedimento;
+import br.com.gbvbahia.financeiro.utils.I18N;
+import br.com.gbvbahia.financeiro.utils.StringBeanUtils;
 import br.com.gbvbahia.financeiro.utils.UtilBeans;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,6 +54,34 @@ public class ProcedimentoBean
     }
 
     @Override
+    public void create(Procedimento entity) throws NegocioException {
+        if (entity instanceof DespesaParceladaProcedimento) {
+            throw new NegocioException(I18N.getMsg("despesaParProcedCreate"));
+        }
+        super.create(entity);
+    }
+
+    @Override
+    public void create(final Procedimento entity,
+            final int parTotal, final int parAtual,
+            final CartaoCredito cartao) throws NegocioException {
+        validarParcelas(parTotal, parAtual);
+        int parcelaAtual = parAtual;
+        String idU = idParcelamento(entity);
+        while (parcelaAtual <= parTotal) {
+            DespesaParceladaProcedimento dpp =
+                    criarDespesaParcelada(entity, parcelaAtual,
+                    parTotal, cartao);
+            if (parcelaAtual++ > parAtual) {
+                dpp.setStatusPagamento(StatusPagamento.NAO_PAGA);
+            }
+            dpp.setIdentificador(idU);
+            super.create(dpp);
+        }
+
+    }
+
+    @Override
     public List<DespesaProcedimento> buscarDespesaProcedimento(
             final CartaoCredito cartao, final StatusPagamento status,
             final Usuario usuario) {
@@ -69,5 +101,59 @@ public class ProcedimentoBean
             toReturn.add((DespesaProcedimento) p);
         }
         return toReturn;
+    }
+
+    /**
+     * Valida as parcelas:<br> Total de parcelas não pode ser menor
+     * que 2.<br> A parcela atual não pode ser menor que 1.<br> O
+     * total de parcelas não pode ser menor que a parcela atual.<br>
+     *
+     * @param parAtual Parcela atual.
+     * @param parTotal Parcela total.
+     * @throws NegocioException se as parcelas não estiverem em
+     * conformidade.
+     */
+    private void validarParcelas(final int parTotal,
+            final int parAtual) throws NegocioException {
+        if (parTotal < 2 || parAtual < 1 || parTotal < parAtual) {
+            throw new NegocioException("parcelasInvalidas",
+                    new String[]{parAtual + "", parTotal + ""});
+        }
+    }
+
+    /**
+     * Cria um ID único para as parcelas geradas.
+     *
+     * @param entity
+     * @return String Id unico.
+     */
+    private String idParcelamento(final Procedimento entity) {
+        final String idU =
+                StringBeanUtils.getIdentificadorUnico(
+                entity.getUsuario().getUserId(),
+                entity.getDataVencimento());
+        return idU;
+    }
+
+    /**
+     * Cria a despesa parcelada de acordo com os parâmetros.
+     *
+     * @param entity Procedimento de origem. Obrigatório.
+     * @param parAtual Parcela atual. Obrigatório.
+     * @param parTotal Parcela total. Obrigatório.
+     * @param cartao Cartão onde parcelamento foi realizado. Opcional.
+     * @return DespesaParceladaProcedimento crada.
+     */
+    private DespesaParceladaProcedimento criarDespesaParcelada(
+            final Procedimento entity, final int parAtual,
+            final int parTotal, final CartaoCredito cartao) {
+        DespesaParceladaProcedimento dpp =
+                new DespesaParceladaProcedimento(entity);
+        dpp.setParcelaAtual(parAtual);
+        dpp.setParcelaTotal(parTotal);
+        if (cartao != null) {
+            dpp.setCartaoCredito(cartao);
+        }
+        return dpp;
     }
 }
