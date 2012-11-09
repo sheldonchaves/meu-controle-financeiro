9e30cbfcaae30a4c078c4474086426b56c8e9b3f
gbvbahia01 <gbvbahia01@gmail.com>
2012-11-09 14:46:55 +0000

    Agenda Table

--- Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/AgendaProcedimentoFixoBean.java
+++ Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/AgendaProcedimentoFixoBean.java
@@ -7,9 +7,15 @@ package br.com.gbvbahia.financeiro.beans;
 import br.com.gbvbahia.financeiro.beans.aop.LogTime;
 import br.com.gbvbahia.financeiro.beans.commons.AbstractFacade;
 import br.com.gbvbahia.financeiro.beans.facades.AgendaProcedimentoFixoFacade;
+import br.com.gbvbahia.financeiro.constantes.Periodo;
+import br.com.gbvbahia.financeiro.constantes.TipoProcedimento;
 import br.com.gbvbahia.financeiro.modelos.AgendaProcedimentoFixo;
+import br.com.gbvbahia.financeiro.modelos.DetalheProcedimento;
+import br.com.gbvbahia.financeiro.modelos.Usuario;
 import br.com.gbvbahia.financeiro.utils.UtilBeans;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
@@ -24,15 +30,14 @@ import javax.persistence.Query;
  * @since 01/04/2012
  */
 @Stateless
-@RolesAllowed({ "admin", "user" })
+@RolesAllowed({"admin", "user"})
 @Interceptors({LogTime.class})
 public class AgendaProcedimentoFixoBean
         extends AbstractFacade<AgendaProcedimentoFixo, Long>
         implements AgendaProcedimentoFixoFacade {
 
     /**
-     * Construtor padrão que passa o tipo de classe para
-     * AbstractFacade.
+     * Construtor padrão que passa o tipo de classe para AbstractFacade.
      */
     public AgendaProcedimentoFixoBean() {
         super(AgendaProcedimentoFixo.class);
@@ -49,10 +54,45 @@ public class AgendaProcedimentoFixoBean
     }
 
     @Override
+    @Interceptors({LogTime.class})
     public Date buscarUltimaData(final AgendaProcedimentoFixo agenda) {
         UtilBeans.checkNull(agenda);
         Query q = em.createNamedQuery("AgendaProcedimentoFixo.UltimaData");
         q.setParameter("agenda", agenda);
         return (Date) q.getSingleResult();
+    }
+
+    @Override
+    @Interceptors({LogTime.class})
+    public Long countarAgendaPorUserDetalhePeriodoTipo(final Usuario user,
+            final DetalheProcedimento detalhe,
+            final Periodo periodo, final TipoProcedimento tipo) {
+        Map<String, Object> parans = getMapParans();
+        populateParans(parans, user, detalhe, periodo, tipo);
+        return pesqCount("AgendaProcedimentoFixo.countDetalhePeriodoTipo", parans);
+    }
+
+    @Override
+    @Interceptors({LogTime.class})
+    public List<AgendaProcedimentoFixo> buscarAgendaPorUserDetalhePeriodoTipoPaginado(
+            final Usuario user, final DetalheProcedimento detalhe,
+            final Periodo periodo, final TipoProcedimento tipo,
+            final int[] range) {
+        Map<String, Object> parans = getMapParans();
+        populateParans(parans, user, detalhe, periodo, tipo);
+        return listPesqParam("AgendaProcedimentoFixo.selectDetalhePeriodoTipo",
+                parans, range[1] - range[0], range[0]);
+    }
+
+    private void populateParans(Map<String, Object> parans, final Usuario user,
+            final DetalheProcedimento detalhe, final Periodo periodo,
+            final TipoProcedimento tipo) {
+        parans.put("usuario", user);
+        parans.put("detalhe", detalhe);
+        parans.put("detalhe2", detalhe == null ? "todos" : "filtro");
+        parans.put("periodo", periodo);
+        parans.put("periodo2", periodo == null ? "todos" : "filtro");
+        parans.put("tipo", tipo);
+        parans.put("tipo2", tipo == null ? "todos" : "filtro");
     }
 }
