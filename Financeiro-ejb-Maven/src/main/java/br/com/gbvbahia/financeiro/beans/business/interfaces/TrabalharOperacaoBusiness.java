209b3b37179e4dfec5be7ec6f67b9af3c18e8458
gbvbahia01 <gbvbahia01@gmail.com>
2012-11-15 01:43:06 +0000

    Movimentação cartao

--- Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/business/interfaces/TrabalharOperacaoBusiness.java
+++ Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/business/interfaces/TrabalharOperacaoBusiness.java
@@ -7,6 +7,7 @@ package br.com.gbvbahia.financeiro.beans.business.interfaces;
 import br.com.gbvbahia.financeiro.beans.exceptions.NegocioException;
 import br.com.gbvbahia.financeiro.modelos.ContaBancaria;
 import br.com.gbvbahia.financeiro.modelos.Procedimento;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -17,6 +18,16 @@ import javax.ejb.Local;
 public interface TrabalharOperacaoBusiness {
 
     /**
+     * Marca varias contas, procedimentos, como paga, cria a movimentação
+     * financeira e altera o saldo da conta bancaria, disponível, debitado.
+     *
+     * @param procedimentos
+     * @param disponivel
+     * @throws NegocioException
+     */
+    void fecharOperacoes(List<Procedimento> procedimentos, ContaBancaria disponivel) throws NegocioException;
+
+    /**
      * Marca uma conta, procedimento, como paga, cria a movimentação
      * financeira e altera o saldo da conta bancaria, disponível, debitado.
      *
@@ -24,7 +35,7 @@ public interface TrabalharOperacaoBusiness {
      * @param disponivel Obrigatorio.
      * @throws NegocioException
      */
-    public void fecharOperacao(Procedimento procedimento, ContaBancaria disponivel) throws NegocioException;
+    void fecharOperacao(Procedimento procedimento, ContaBancaria disponivel) throws NegocioException;
 
     /**
      * Marca uma conta, procedimento, como aberta, apaga a amovimentação
@@ -35,5 +46,16 @@ public interface TrabalharOperacaoBusiness {
      * @param procedimento Obrigatorio.
      * @throws NegocioException
      */
-    public void abrirOperacao(Procedimento procedimento) throws NegocioException;
+    void abrirOperacao(Procedimento procedimento) throws NegocioException;
+
+    /**
+     * Marca varias contas, procedimentos, como aberta, apaga a amovimentação
+     * financeira e altera o saldo da conta bancária. Se não encontrar a
+     * movimentação financeira, altera somente o status da conta para não
+     * paga.
+     *
+     * @param procedimento Obrigatorio.
+     * @throws NegocioException
+     */
+    void abrirOperacoes(List<Procedimento> procedimentos) throws NegocioException;
 }
