c77f66e06668e61ca4e61404590678ba3f780cee
gbvbahia01 <gbvbahia01@gmail.com>
2012-11-19 21:20:32 +0000

    Crud agenda

---
+++ Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/SchedulerBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gbvbahia.financeiro.beans;
+
+import br.com.gbvbahia.financeiro.beans.commons.AbstractFacade;
+import br.com.gbvbahia.financeiro.beans.exceptions.NegocioException;
+import br.com.gbvbahia.financeiro.beans.facades.SchedulerFacade;
+import br.com.gbvbahia.financeiro.modelos.Scheduler;
+import br.com.gbvbahia.financeiro.modelos.Usuario;
+import br.com.gbvbahia.financeiro.utils.UtilBeans;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Usuário do Windows
+ */
+@Stateless
+public class SchedulerBean extends AbstractFacade<Scheduler, Long> implements SchedulerFacade {
+
+    /**
+     * Unidade de persistência <i>jdbc/money</i>.
+     */
+    @PersistenceContext(unitName = UtilBeans.PERSISTENCE_UNIT)
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SchedulerBean() {
+        super(Scheduler.class);
+    }
+
+    @Override
+    public Scheduler buscarSchedulerPorUsuario(Usuario usuario) {
+        Map<String, Object> parans = AbstractFacade.getMapParans();
+        parans.put("user", usuario);
+        return pesqParam("SchedulerBean.buscarSchedulerPorUsuario", parans);
+    }
+
+    @Override
+    public List<Scheduler> buscarTodosSchelersPorStatus(final boolean status) {
+        Map<String, Object> parans = AbstractFacade.getMapParans();
+        parans.put("status", status);
+        return listPesqParam("SchedulerBean.buscarTodosSchelersPorStatus",
+                parans);
+    }
+
+    @Override
+    public void create(Scheduler entity) throws NegocioException {
+        if (entity.getId() == null) {
+            super.create(entity);
+        } else {
+            super.update(entity);
+        }
+    }
+}
---
+++ Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/modelos/Scheduler.java
@@ -0,0 +1,154 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gbvbahia.financeiro.modelos;
+
+import br.com.gbvbahia.financeiro.modelos.commons.EntityInterface;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Usuário do Windows
+ */
+@Entity
+@Table(name = "fin_scheduler")
+@NamedQueries({
+    @NamedQuery(name = "SchedulerBean.buscarSchedulerPorUsuario",
+    query = " Select s From Scheduler s "
+    + " Where s.user = :user "),
+    @NamedQuery(name = "SchedulerBean.buscarTodosSchelersPorStatus",
+    query = " Select s From Scheduler s "
+    + " Where s.status = :status ")
+})
+public class Scheduler implements EntityInterface<Scheduler>, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    /**
+     *
+     */
+    @Column(name = "nm_dias_vencimento", nullable = false)
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private int dias = 0;
+    /**
+     *
+     */
+    @Column(name = "ds_email_contato", nullable = false, length = 255)
+    @NotNull
+    @Size(max = 255)
+    @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`"
+    + "{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:"
+    + "[a-z0-9-]*[a-z0-9])?",
+    message = "{email.invalido}")
+    private String email;
+    /**
+     *
+     */
+    @Column(name = "fl_status_aviso", nullable = false)
+    private boolean status = true;
+    /**
+     *
+     */
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "fk_usuario", referencedColumnName = "user_id", nullable = false)
+    private Usuario user;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scheduler other = (Scheduler) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getLabel() {
+        return dias + " | " + email;
+    }
+
+    @Override
+    public boolean verificarId() {
+        return false;
+    }
+
+    @Override
+    public int compareTo(Scheduler o) {
+        return email.compareTo(o.email);
+    }
+}
