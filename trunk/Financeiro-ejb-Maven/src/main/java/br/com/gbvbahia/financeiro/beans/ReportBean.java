8812143b6a08f48b3bb341f324834697fe6e644f
gbvbahia01@gmail.com <gbvbahia01@gmail.com@133ebd45-cf3e-885a-2d9d-162f8e9bdabc>
2012-11-23 01:06:06 +0000

    Acumulado mensal

--- trunk/Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/ReportBean.java
+++ trunk/Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/ReportBean.java
@@ -5,12 +5,14 @@
 package br.com.gbvbahia.financeiro.beans;
 
 import br.com.gbvbahia.financeiro.beans.facades.ReportFacade;
+import br.com.gbvbahia.financeiro.constantes.TipoProcedimento;
 import br.com.gbvbahia.financeiro.modelos.CartaoCredito;
 import br.com.gbvbahia.financeiro.modelos.Usuario;
 import br.com.gbvbahia.financeiro.utils.DateUtils;
 import br.com.gbvbahia.financeiro.utils.UtilBeans;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +45,21 @@ public class ReportBean implements ReportFacade {
         Map<CartaoCredito, Double> toReturn = new HashMap<CartaoCredito, Double>();
         for (Object[] valor : valores) {
             toReturn.put((CartaoCredito) valor[0], ((BigDecimal) valor[1]).doubleValue());
+        }
+        return toReturn;
+    }
+    
+    @Override
+    public Map<TipoProcedimento, Double> acumuladoReceitaDespesaPeriodo(Date mesAno, Usuario usuario){
+        Date[] periodo = DateUtils.primeiroUltimoDia(mesAno);
+        Query q = em.createNamedQuery("Procedimento.acumuladoReceitaDespesaPeriodo");
+        q.setParameter("dataI", periodo[0]);
+        q.setParameter("dataF", periodo[1]);
+        q.setParameter("usuario", usuario);
+        List<Object[]> valores = q.getResultList();
+        Map<TipoProcedimento, Double> toReturn = new EnumMap<TipoProcedimento, Double>(TipoProcedimento.class);
+         for (Object[] valor : valores) {
+            toReturn.put((TipoProcedimento) valor[0], ((BigDecimal) valor[1]).doubleValue());
         }
         return toReturn;
     }
