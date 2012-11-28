39639a3c51978c6b3493c3051c4dbae0f9f460f8
gbvbahia01 <gbvbahia01@gmail.com>
2012-11-28 16:28:53 +0000

    Aviso e-mail

--- Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/business/scheduler/AvisoVencimentosBean.java
+++ Financeiro-ejb-Maven/src/main/java/br/com/gbvbahia/financeiro/beans/business/scheduler/AvisoVencimentosBean.java
@@ -50,8 +50,8 @@ public class AvisoVencimentosBean implements AvisoVencimentosBusiness {
     private EmailSendBusiness emailSendBusiness;
 
     @Override
-    @Schedule(hour = "*", minute = "*", second = "10", dayOfWeek = "*")//Teste
-    //@Schedule(hour = "4", minute = "23", second = "10", dayOfWeek = "*")//Real
+    //@Schedule(hour = "*", minute = "*", second = "10", dayOfWeek = "*")//Teste
+    @Schedule(hour = "4", minute = "23", second = "10", dayOfWeek = "*")//Real
     public void iniciarAvisoVencimento() {
         List<Scheduler> schedules = schedulerBean.buscarTodosSchelersPorStatus(true);
         Calendar[] intervalo = getIntervalo();
